180a48f8acfc7333dde1f1d519ac636c33b501c0
0mariapaula <anavanessa162@gmail.com>
2023-12-07 11:38:23 -0300

    Atu07

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,16 +9,40 @@ export const metadata = {
 
 function Logo({x, y}){
   return (
-    <p style={{left: x, top: y }} className={styles.logo}>Ready<span className={styles.comercial}>&</span>Go</p>
-    <Image></Image>
+    <p style={{left: x, top: y }} className={styles.logo}>Me<></>Weeb<span></span></p>
+    
   );
 }
+
+function ItemMenu({nome}){
+  return(
+   
+    <p className={styles.itemMenu}>{nome}</p>
+  )
+}
+
+function Premium(){
+  return(
+    <div>
+      <Image></Image>
+      <p>Premium</p>
+    </div>
+  )
+} 
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
         <Logo></Logo>
+        <ItemMenu nome="Genre"/>
+        <ItemMenu nome="Movies"/>
+        <ItemMenu nome="ONAs"/>
+        <ItemMenu nome="News"/>
+        <Premium></Premium>
+        <Image></Image>
+
+        {/* <Filme imagem={titans} numero="01" nome="Attack on Titan"/> */}
       </body>
     </html>
   )
